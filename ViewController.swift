cfb8b020eb61f3278cba46aa60b37e08a604315e
Masaki Kanto <kevjet@wlan-napt-001.u-aizu.ac.jp>
2018-05-31 17:08:44 +0900

    first comit

---
+++ ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Introductoin
+//
+//  Created by Masaki Kanto on H30/05/31.
+//  Copyright © 平成30年 masakikanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+   
+   
+   
+    
+    
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
